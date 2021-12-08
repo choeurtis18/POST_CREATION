f7d79ebd0cdfe7890145aa27e0d9dde70f121c6b
Choeurtis <choeurtis.tchounga@gmail.com>
2021-12-08 15:19:33 +0100

    All Changes, Update, Edit, Rewite Edit Error Controleur(Test)

--- app/src/Controller/FrontController.php
+++ app/src/Controller/FrontController.php
@@ -3,21 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Author;
+use App\Entity\Comment;
+use App\Entity\Post;
 use App\Fram\Factories\PDOFactory;
 use App\Fram\Utils\Flash;
 use App\Manager\PostManager;
+use App\Manager\CommentManager;
 
 class FrontController extends BaseController
 {
     public function executeHome()
     {
         Flash::setFlash('alert', 'je suis une alerte');
-        $postManager = new PostManager(PDOFactory::getMysqlConnection());
-        $posts = $postManager->getAllPosts();
+        $commentManager = new CommentManager(PDOFactory::getMysqlConnection());
+
+        $com = new Comment(["id"=>1, "publishedDate"=>"2021-12-08", "content"=>"s", "authorId"=>1]);
+        $commentManager->updateComment($com);
+        $com = $commentManager->deleteCommentById(1);
+
         $this->render(
             'home.php',
             [
-                'posts' => $posts
+                'com' => $com,
             ],
             'Home Page'
         );
--- app/src/Views/Frontend/404.php
+++ app/src/Views/Frontend/404.php
@@ -1,1 +1,2 @@
 <h1>Vous vous êtes planté !</h1>
+<p><?php echo $error ?></p>
